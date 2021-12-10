ad381f71317b90184402910e6808e01b00a306e2
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-12-10 10:29:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const closedFace = document.querySelector('.closed') ;
 const openFace = document.querySelector('.open');
 const mouthFace = document.querySelector('.mouth');
 
+const heartFace = document.querySelector('.heartface');
+const heartEyes = document.querySelector('.hearteyes');
+const starEyes = document.querySelector('.stareyes');
