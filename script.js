76c7eae8f8cb2ef520a6230725322f02148a93c4
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-12-11 02:17:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const mouthFace = document.querySelector('.mouth');
 const heartFace = document.querySelector('.heartface');
 const heartEyes = document.querySelector('.hearteyes');
 const starEyes = document.querySelector('.stareyes');
+
+const kissed = document.querySelector('.kiss');
+const laughed = document.querySelector('.laugh');
+const tasted = document.querySelector('.tasty');
+
